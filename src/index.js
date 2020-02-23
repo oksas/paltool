9956f433e6489d96fdb6ad7e73a1a58b5e86d7a5
oksas <john.oksasoglu@gmail.com>
2020-02-22 20:45:42 -0800

    Install react-toast-notifications

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import { ToastProvider } from 'react-toast-notifications';
+
 import './styles/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </Provider>,
   document.getElementById('root')
 );
